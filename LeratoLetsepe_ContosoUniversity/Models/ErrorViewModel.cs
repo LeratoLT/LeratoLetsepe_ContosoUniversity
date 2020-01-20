f6d87e62340f38c2a00e8867d8c7692a84e99577
Lerato Letsepe <59874274+rabbit-cryptic@users.noreply.github.com>
2020-01-20 11:12:50 +0200

    Adding vs project

---
+++ LeratoLetsepe_ContosoUniversity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LeratoLetsepe_ContosoUniversity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
